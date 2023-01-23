4046122531f76197df7080ff4a246061e7945e9a
ProjectKey007 <120080399+ProjectKey007@users.noreply.github.com>
2023-01-23 16:46:02 +0400

    Add files via upload

---
+++ Key 1.3V/Data.js
@@ -0,0 +1,15 @@
+// Define a list of translations
+const translations = {
+  "book": "գիրք",
+  "reading": "կարդալ",
+  "page": "էջ",
+  "Great": "մեծն",
+  "story": "Պատմություն",
+  "summer": "Ամառ",
+  "Boy": "տղա",
+  "map": "Քարտեզ",
+  "and": "և",
+  "four": "Չորս",
+  "four,": "Չորս",
+  "man": "Տղամարդ, մարդ"
+};
