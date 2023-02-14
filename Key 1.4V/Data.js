9f004686a394c28110a5028b64b68d4e42f734f3
ProjectKey007 <120080399+ProjectKey007@users.noreply.github.com>
2023-02-14 15:24:16 +0400

    Add files via upload

---
+++ Key 1.4V/Data.js
@@ -0,0 +1,36 @@
+const translations = new Map([
+  ["book", "գիրք"],
+  ["reading", "կարդալ"],
+  ["page", "էջ"],
+  ["Great", "մեծն"],
+  ["story", "Պատմություն"],
+  ["summer", "Ամառ"],
+  ["Boy", "տղա"],
+  ["map", "Քարտեզ"],
+  ["and", "և"],
+  ["four", "Չորս"],
+  ["four,", "Չորս"],
+  ["man", "Տղամարդ, մարդ"]
+]);
+
+
+
+// Define a list of translations
+//const translations = {
+ // "book": "գիրք",
+ // "reading": "կարդալ",
+ // "page": "էջ",
+ // "Great": "մեծն",
+ // "story": "Պատմություն",
+ //"summer": "Ամառ",
+ // "Boy": "տղա",
+//"map": "Քարտեզ",
+ // "and": "և",
+ // "four": "Չորս",
+ // "four,": "Չորս",
+ // "man": "Տղամարդ, մարդ"
+//};
+
+//function fun() {
+ // translations.set("all", "all");
+//}
---
+++ Key 1.4V/Duglas.js
@@ -0,0 +1,38 @@
+// Select all text elements in the HTML book
+const textElements = document.querySelectorAll("p, h1, h2, span");
+
+// Loop through each text element
+textElements.forEach(element => {
+  // Split the text content into words
+  const words = element.textContent.split(" ");
+  
+  // Loop through each word
+  words.forEach((word, index) => {
+    // Check if the word is in the translations list
+    if (translations[word]) {
+      // Replace the word with a translated version
+      words[index] = `<span class="translated" title="${translations[word]}">${word}</span>`;
+    }
+  });
+  
+  // Update the text content with the translated words
+  element.innerHTML = words.join(" ");
+});
+
+// Select all translated elements in the HTML book
+const translatedElements = document.querySelectorAll(".translations");
+
+// Loop through each translated element
+translatedElements.forEach(element => {
+  // Add a click event listener to the element
+  element.addEventListener("click", function() {
+    // Get the word from the element's text content
+    const word = this.textContent;
+    // Play the audio file of the word's pronunciation
+    new Audio(`audio/${word}.mp3`).play();
+  });
+});
+
+function fun() {
+  alert('hello');
+}
